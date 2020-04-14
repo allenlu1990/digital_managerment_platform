2397988603b02ac140130225bcf2a24b95918481
alu <alu@Allens-MacBook.lan>
2020-04-14 09:56:27 +0800

    create

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard-editor-container">
+    <el-row ::gutter="15">
+      <el-col :span="16" :offset="2">
+        <div class="grid-content bg-purple" justify="center">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span></span>
+            </div>
+             <div class="notice_list">  
+                <p>上海技术物理研究所第二研究室光谱成像组数字化平台</p>
+             </div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .box-card {
+    width: 80%; 
+    margin: 0 auto;
+  }
+</style>
